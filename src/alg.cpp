062d97d614e5bb4dccd267a91c76224d7d607f52
kre1m <aksenovnikita2006@gmail.com>
2025-05-15 16:30:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,77 @@
 // Copyright 2025 NNTU-CS
 #include <string>
 #include <map>
+#include <cctype>
+#include <sstream>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int priority(char op) {
+  if (op == '+' || op == '-') return 1;
+  if (op == '*' || op == '/') return 2;
+  return 0;  
 }
 
-int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+std::string infx2pstfx(const std::string& inf) { 
+  TStack<char, 100> stack;
+  std::string output;
+  std::string num;
+  for (size_t i = 0; i < inf.length(); ++i) {
+    char c = inf[i];
+    if (isdigit(c)) {
+      num += c;
+    } else {
+      if (!num.empty()) {
+        output += num + ' ';
+        num.clear();
+      }
+      if (c == '(') {
+        stack.push(c);
+      } else if (c == ')') {
+        while (!stack.isEmpty() && stack.top() != '(') {
+          output += stack.pop();
+          output += ' ';
+        }
+        stack.pop();
+      } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+        while (!stack.isEmpty() && priority(stack.top()) >= priority(c)) {
+          output += stack.pop();
+          output += ' ';
+        }
+        stack.push(c);
+      }
+    }
+  }
+  if (!num.empty()) {
+    output += num + ' ';
+  }
+  while (!stack.isEmpty()) {
+    output += stack.pop();
+    output += ' ';
+  }
+  if (!output.empty() && output.back() == ' ')
+    output.pop_back();
+  return output;
+}
+
+int eval(const std::string& post) {
+  TStack<int, 100> stack;
+  std::istringstream iss(post);
+  std::string token;
+  while (iss >> token) {
+    if (isdigit(token[0])) {
+      stack.push(std::stoi(token));
+    } else {
+      int b = stack.pop();
+      int a = stack.pop();
+      int res = 0;
+      switch (token[0]) {
+        case '+': res = a + b; break;
+        case '-': res = a - b; break;
+        case '*': res = a * b; break;
+        case '/': res = a / b; break;
+      }
+      stack.push(res);
+    }
+  }
+  return stack.pop();
 }
